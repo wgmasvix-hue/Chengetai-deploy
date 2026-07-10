ee28c69ca2bbb2b07864f61496253afab54c4605
root <root@vmi3395650.contaboserver.net>
2026-07-10 02:09:42 +0200

    Initialize ChengetAI Deploy Go CLI

---
+++ cli/cmd/chengetai/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("ChengetAI Deploy v2")
+	fmt.Println("Enterprise AI Deployment Platform")
+}
---
+++ cli/go.mod
@@ -0,0 +1,3 @@
+module github.com/wgmasvix-hue/chengetai-deploy/cli
+
+go 1.25.0
